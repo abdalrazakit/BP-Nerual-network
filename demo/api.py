ed2da2a0125c9a9c645b9e1978b1a8d6b0e6bd61
abdal <abdalrazak.it@gmail.com>
2022-05-12 10:35:44 +0300

    fix api

--- demo/api.py
+++ demo/api.py
@@ -8,6 +8,7 @@ import time
 import warnings
 import cv2
 import tqdm
+import requests
 
 from detectron2.config import get_cfg
 from detectron2.data.detection_utils import read_image
@@ -19,6 +20,8 @@ from detectron2.engine.defaults import DefaultPredictor
 from detectron2.utils.video_visualizer import VideoVisualizer
 from detectron2.utils.visualizer import ColorMode, Visualizer
 from predictor import VisualizationDemo
+from detectron2.data import DatasetCatalog
+from datetime import datetime
 
 from typing import Optional
 
@@ -26,25 +29,45 @@ from fastapi import FastAPI
 
 app = FastAPI(debug=True)
 
-def callback(id: int, count: int, time: str):
-    print("Sum = {}".format(count))
+
+def callback(id: int, count: int,degree: int, out):
+    global URL
+    print("send request to backend module")
+    URL = "http://server.yesilkalacak.com/api/camera/addCameraReport"
+    date_time = datetime.now()
+    str_date_time = date_time.strftime("%d-%m-%Y-%H:%M:%S.%f")
+    path = "/home/server/public_html/storage/app/public/stream/result/" + str(id);
+    if not os.path.exists(path):
+        os.makedirs(path)
+    filename = path + "/" + str_date_time + ".png"
+    out.save(filename)
+    data = {
+        'stream': id,
+        'description': "detected {} instances".format(count),
+        'path': "stream/result/" + str(id) + "/" + str_date_time + ".png",
+        'degree': degree,
+        'count': count
+    }
+    r = requests.post(url=URL, data=data)
+    #print(r.status_code)
+    #print(r.text)
+
 
 @app.get("/stream/{id}")
 def stream(id: int, path: str, showDetectWindwos: bool = False):
-    check_media(path, showDetectWindwos, callback)
-    return {
-        "result": "process done"
-    }
+    return check_media(path, showDetectWindwos, id, callback)
 
-@app.get("/check/{path}")
-def check_media(path: str, showDetectWindwos=False, cameraID=0, callBack=None):
+
+@app.get("/check")
+def check_media(path: str, showDetectWindwos=False, cameraID=None, callBack=None):
     mp.set_start_method("spawn", force=True)
     logger = setup_logger()
 
     cfg = get_cfg()
     cfg.merge_from_file("configs/COCO-Detection/faster_rcnn_R_50_FPN_1x.yaml")
 
-    register_coco_instances("my_dataset_test", {}, "content/test/_annotations.coco.json", "content/test")
+    if "my_dataset_test" not in DatasetCatalog.list():
+        register_coco_instances("my_dataset_test", {}, "content/test/_annotations.coco.json", "content/test")
     cfg.DATASETS.TEST = ("my_dataset_test",)
     cfg.MODEL.WEIGHTS = os.path.join("model_final.pth")
 
@@ -52,14 +75,17 @@ def check_media(path: str, showDetectWindwos=False, cameraID=0, callBack=None):
     cfg.MODEL.ROI_HEADS.NUM_CLASSES = 3  # your number of classes + 1
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7  # set the testing threshold for this model
     cfg.MODEL.DEVICE = "cpu"
-    cfg.TEST.DETECTIONS_PER_IMAGE = 100
-    cfg.INPUT.MIN_SIZE_TEST = 0
+    cfg.TEST.DETECTIONS_PER_IMAGE = 10
+    cfg.INPUT.MIN_SIZE_TEST = 200
+    cfg.INPUT.MAX_SIZE_TEST = 500
     predictor = DefaultPredictor(cfg)
     test_metadata = MetadataCatalog.get("my_dataset_test")
 
     if ".jpg" in path:
-        imageName = path
-        im = cv2.imread('content/input/' + imageName)
+        imageName = "/home/server/public_html/storage/app/public/" + path
+
+        im = cv2.imread( imageName)
+
         start_time = time.time()
         outputs = predictor(im)
         logger.info("{}:  -----  {} in {:.2f}s".format(
@@ -71,36 +97,94 @@ def check_media(path: str, showDetectWindwos=False, cameraID=0, callBack=None):
 
         v = Visualizer(im[:, :, ::-1], metadata=test_metadata)
         out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-        out.save(os.path.join("content/out/", imageName))
+        out.save(imageName  + "RES.jpg")
 
         return {
             "detect": True if len(outputs["instances"]) > 0 else False,
-            "count": len(outputs["instances"]),
             "time": took_time,
+            "detect_fires": len(outputs["instances"]) if len(outputs["instances"]) > 0 else 0,
+            "decree": int(outputs["instances"].scores[0] * 100) if len(outputs["instances"]) > 0 else 0
         }
 
-    if ".mp4" in path:
-        video_path = path
-        video = cv2.VideoCapture('content/input/' + video_path)
+    if cameraID:
+        video = cv2.VideoCapture(path)
         frames_per_second = video.get(cv2.CAP_PROP_FPS) / 10
-        num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT)) / 10
-        output_fname = os.path.splitext(os.path.join("content/out/", video_path))[0] + ".mp4"
+        video_visualizer = VideoVisualizer(test_metadata)
 
-        output_file = cv2.VideoWriter(filename=output_fname, fourcc=cv2.VideoWriter_fourcc(*"mp4v"),
-                                      fps=float(frames_per_second), frameSize=(
-                int(video.get(cv2.CAP_PROP_FRAME_WIDTH)), int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))),
+        count = 0
+        found_counter = 0
+        processed_counter = 0
+        while video.isOpened():
+            success, frame = video.read()
+            if success:
+                count = count + 1
+                if count % 10 == 0:
+                    processed_counter = processed_counter + 1
+                    start_time = time.time()
+                    outputs = predictor(frame)
+                    logger.info(
+                        "frame: {}:  -----  {} in {:.2f}s".format(
+                            count,
+                            "detected {} instances".format(len(outputs["instances"])),
+                            time.time() - start_time,
+                        )
+                    )
+
+                    if len(outputs["instances"]) > 0:
+                        found_counter = found_counter + 1
+
+                    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                    out = video_visualizer.draw_instance_predictions(frame, outputs["instances"].to("cpu"))
+
+                    if callback:
+                        if len(outputs["instances"]) > 0:
+                            callback(cameraID, len(outputs["instances"]),int(outputs["instances"].scores[0] * 100), out)
+
+                    if (showDetectWindwos):
+                        cv2.namedWindow("COCO detections", cv2.WINDOW_NORMAL)
+                        cv2.imshow("COCO detections", out)
+                        if cv2.waitKey(1) == 27:
+                            break  # esc to quit
+            else:
+                break
+
+        video.release()
+
+        if (showDetectWindwos):
+            cv2.destroyAllWindows()
+        return {
+            "all_frames": count,
+            "processed_frames": processed_counter,
+            "detect_fires": found_counter,
+        }
+
+
+    if ".mp4" in path:
+        path = "/home/server/public_html/storage/app/public/" + path
+        video_out_path =  path + "RES.mp4"
+
+        video = cv2.VideoCapture(path)
+        frames_per_second = video.get(cv2.CAP_PROP_FPS) / 10
+
+        output_file = cv2.VideoWriter(filename=video_out_path, fourcc=cv2.VideoWriter_fourcc(*"mp4v"),
+                                      fps=float(frames_per_second), frameSize=
+                                      (int(video.get(cv2.CAP_PROP_FRAME_WIDTH)),
+                                       int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))),
                                       isColor=True, )
 
         video_visualizer = VideoVisualizer(test_metadata)
 
         count = 0
         found_counter = 0
+        processed_counter = 0
+        decree = 0
         logs = []
         while video.isOpened():
             success, frame = video.read()
-            count = count + 1
             if success:
+                count = count + 1
                 if count % 10 == 0:
+                    processed_counter = processed_counter + 1
                     start_time = time.time()
                     outputs = predictor(frame)
                     logger.info(
@@ -115,49 +199,32 @@ def check_media(path: str, showDetectWindwos=False, cameraID=0, callBack=None):
                         "detected {} instances".format(len(outputs["instances"])),
                         time.time() - start_time,
                     ))
-                    if len(outputs["instances"]) > 0:
-                        found_counter = found_counter + 1
 
-                    if callback:
-                        callback(cameraID, len(outputs["instances"]), time.time())
+                    if len(outputs["instances"]) > 0:
+                        found_counter = found_counter + len(outputs["instances"])
+                        if int(outputs["instances"].scores[0] * 100) > decree:
+                            decree = int(outputs["instances"].scores[0] * 100)
 
                     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                     out = video_visualizer.draw_instance_predictions(frame, outputs["instances"].to("cpu"))
+
                     out = cv2.cvtColor(out.get_image(), cv2.COLOR_RGB2BGR)
-
                     output_file.write(out)
-                    if (showDetectWindwos):
-                        cv2.namedWindow("COCO detections", cv2.WINDOW_NORMAL)
-                        cv2.imshow("COCO detections", out)
-                        if cv2.waitKey(1) == 27:
-                            break  # esc to quit
 
-                    if found_counter > 10:
-                        video.release()
-                        output_file.release()
-                        if (showDetectWindwos):
-                            cv2.destroyAllWindows()
-                        return {
-                            "all_frames": num_frames * 10,
-                            "processed_frames": count,
-                            "check_frames": count / 10,
-                            "detect_frames": found_counter,
-                            "logs": logs
-                        }
             else:
                 break
 
         video.release()
         output_file.release()
-        if (showDetectWindwos):
-            cv2.destroyAllWindows()
+        cv2.destroyAllWindows()
         return {
-            "all_frames": num_frames * 10,
-            "processed_frames": count,
-            "check_frames": count / 10,
-            "detect_frames": found_counter,
+            "all_frames": count,
+            "processed_frames": processed_counter,
+            "detect_fires": found_counter,
+            "decree": decree,
             "logs": logs
         }
+
     return {
         "error": "file type"
     }
